8e947652aa5ff610ddb54867054b025e5f0431bc
Aaron Santiago <me@aaron.work>
2024-08-27 00:09:46 -0400

    Update to add ability/items along with localized names for heroes and abilities/items

--- data-fetcher/decompile.sh
+++ data-fetcher/decompile.sh
@@ -1,1 +1,12 @@
-~/Downloads/Decompiler-windows-x64/Decompiler.exe -i "C:\Program Files (x86)\Steam\steamapps\common\Deadlock\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/heroes.vdata_c" -d
+
+DEADLOCK_LOCATION="D:\SteamLibrary\steamapps\common\Deadlock"
+# DEADLOCK_LOCATION="C:\Program Files (x86)\Steam\steamapps\common\Deadlock"
+
+~/Downloads/Decompiler-windows-x64/Decompiler.exe -i "$DEADLOCK_LOCATION\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/heroes.vdata_c" -d
+~/Downloads/Decompiler-windows-x64/Decompiler.exe -i "$DEADLOCK_LOCATION\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/abilities.vdata_c" -d
+mkdir -p "decompiled-data\localizations"
+cp "$DEADLOCK_LOCATION\game\citadel\resource\localization\citadel_gc\citadel_gc_english.txt" "decompiled-data\localizations\citadel_gc_english.txt"
+python3 localization_parser.py "decompiled-data\localizations\citadel_gc_english.txt" "decompiled-data\localizations\citadel_gc_english.json"
+
+cp "$DEADLOCK_LOCATION\game\citadel\resource\localization\citadel_mods\citadel_mods_english.txt" "decompiled-data\localizations\citadel_mods_english.txt"
+python3 localization_parser.py "decompiled-data\localizations\citadel_mods_english.txt" "decompiled-data\localizations\citadel_mods_english.json"
--- data-fetcher/parser.py
+++ data-fetcher/parser.py
@@ -1,31 +1,75 @@
 import keyvalues3 as kv3
 import json
+import tempfile
+import os
 
 class Parser:
     def __init__(self):
-        self.data = kv3.read('./decompiled-data/scripts/heroes.vdata')
+        self.data_dir = './decompiled-data/'
+        self.hero_data_path = os.path.join(self.data_dir, 'scripts/heroes.vdata')
+        self.abilities_data_path = os.path.join(self.data_dir , 'scripts/abilities.vdata')
+        with open(self.abilities_data_path, 'r') as f:
+            content = f.read()
+            # replace 'subclass:' with ''
+            # subclass features in kv3 don't seem to be supported in the keyvalues3 python library
+            content = content.replace('subclass:', '')
+            # write new content to tempfile
+            with tempfile.NamedTemporaryFile(mode='w', delete=False) as tf:
+                tf.write(content)
+                self.abilities_data = kv3.read(tf.name)
+
+
+        self.hero_data = kv3.read(os.path.join(self.data_dir, 'scripts/heroes.vdata'))
+
+        self.localizations = dict()
+        with open(os.path.join(self.data_dir, 'localizations/citadel_gc_english.json'), 'r') as f:
+            names = json.load(f)
+            self.localizations.update(names)
+        
+        with open(os.path.join(self.data_dir, 'localizations/citadel_mods_english.json'), 'r') as f:
+            descriptions = json.load(f)
+            self.localizations.update(descriptions)
 
     def run(self): 
         self.parse_heroes()
+        self.parse_abilities()
 
     def parse_heroes(self):
-        hero_keys = self.data.keys()
-        base_hero_stats = self.data['hero_base']['m_mapStartingStats']
+        hero_keys = self.hero_data.keys()
+
+        # Base hero stats
+        base_hero_stats = self.hero_data['hero_base']['m_mapStartingStats']
 
         all_hero_stats = dict()
 
         for hero_key in hero_keys:
             if hero_key.startswith('hero') and hero_key != 'hero_base':
+
                 merged_stats = dict()
-                hero_stats = self.data[hero_key]['m_mapStartingStats']
+
+                merged_stats['name'] = self.localizations.get(hero_key, 'Unknown')
+
+                # Hero specific stats applied over base stats
+                hero_stats = self.hero_data[hero_key]['m_mapStartingStats']
                 merged_stats.update(base_hero_stats)
                 merged_stats.update(hero_stats)
 
-                hero_name = hero_key.replace('hero_', '')
-                all_hero_stats[hero_name] = merged_stats
+                all_hero_stats[hero_key] = merged_stats
 
         self.write('output/hero-data.json', all_hero_stats) 
+    
+    def parse_abilities(self):
+        ability_keys = self.abilities_data.keys()
+        all_abilities = dict()
 
+        for ability_key in ability_keys:
+            ability_data = {}
+
+            ability_data['name'] = self.localizations.get(ability_key, 'Unknown')
+
+            all_abilities[ability_key] = ability_data
+
+        self.write('output/ability-data.json', all_abilities)
 
     def write(self, path, data):
         with open(path, "w") as outfile:
