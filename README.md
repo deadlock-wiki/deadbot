9ed5474ee76d86416f83e8d6758fa1f021940169
hassayag <haassayag@gmail.com>
2024-08-28 10:38:38 +0100

    added black for code formatting

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 DeadBot is an automation tool for maintaining the Deadlock Wiki - https://deadlocked.wiki/
 
 ## Setup
-- pip install poetry
-- python -m poetry install
+- `pip install poetry`
+- `python -m poetry install`
+- `python -m pre_commit`
 - Download Decompiler.exe from https://github.com/ValveResourceFormat/ValveResourceFormat/releases
 - Extract decompiler into `Downloads/` folder
 
--- src/parser/localization_parser.py
+++ src/parser/localization_parser.py
@@ -2,18 +2,18 @@ import sys
 import json
 
 if __name__ == "__main__":
-  with open(sys.argv[1], "r") as f:
-      lines = f.readlines()
-      out = dict()
-      for line in lines:
-          if line.count('"') >= 4:
-              left = line.split('"')[1]
+    with open(sys.argv[1], "r") as f:
+        lines = f.readlines()
+        out = dict()
+        for line in lines:
+            if line.count('"') >= 4:
+                left = line.split('"')[1]
 
-              # Handle cases where there are escaped quotations in the string
-              right = "".join(line.split('"')[3:]).strip()
-              right = right.replace("\\", "\"")
+                # Handle cases where there are escaped quotations in the string
+                right = "".join(line.split('"')[3:]).strip()
+                right = right.replace("\\", '"')
 
-              out[left] = right
-              
-      with open(sys.argv[2], "w") as f:
-          json.dump(out, f, indent=4)
+                out[left] = right
+
+        with open(sys.argv[2], "w") as f:
+            json.dump(out, f, indent=4)
