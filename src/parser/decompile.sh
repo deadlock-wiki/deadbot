58a59de36a8a4f0fc9543f33665ba4bcd40db388
Ethan <119145352+Surxe@users.noreply.github.com>
2024-08-30 15:49:29 -0400

    Config paths (#6)
    * Config paths
    
    .env.local added and called instead of absolute paths.
    
    Configs are saved to .env.local only for now, see .env.example for example.
    
    The only config related file moved to src/utils/config_manager.py
    
    Renamed _LOCATION suffixed variables to _PATH suffix

--- src/parser/decompile.sh
+++ src/parser/decompile.sh
@@ -1,16 +1,13 @@
 
-# DEADLOCK_LOCATION="D:\SteamLibrary\steamapps\common\Deadlock"
-DEADLOCK_LOCATION="C:\Program Files (x86)\Steam\steamapps\common\Deadlock"
+. ../../.env.local #Retrieve config paths
 
-DECOMPILER_LOCATION=~/Downloads/Decompiler-windows-x64
-
-$DECOMPILER_LOCATION/Decompiler.exe -i "$DEADLOCK_LOCATION\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/heroes.vdata_c" -d
-$DECOMPILER_LOCATION/Decompiler.exe -i "$DEADLOCK_LOCATION\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/abilities.vdata_c" -d
-$DECOMPILER_LOCATION/Decompiler.exe -i "$DEADLOCK_LOCATION\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/generic_data.vdata_c" -d
+$DECOMPILER_PATH/Decompiler.exe -i "$DEADLOCK_PATH\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/heroes.vdata_c" -d
+$DECOMPILER_PATH/Decompiler.exe -i "$DEADLOCK_PATH\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/abilities.vdata_c" -d
+$DECOMPILER_PATH/Decompiler.exe -i "$DEADLOCK_PATH\game\citadel\pak01_dir.vpk" --output "decompiled-data" --vpk_filepath "scripts/generic_data.vdata_c" -d
 
 mkdir -p "decompiled-data\localizations"
-cp "$DEADLOCK_LOCATION\game\citadel\resource\localization\citadel_gc\citadel_gc_english.txt" "decompiled-data\localizations\citadel_gc_english.txt"
+cp "$DEADLOCK_PATH\game\citadel\resource\localization\citadel_gc\citadel_gc_english.txt" "decompiled-data\localizations\citadel_gc_english.txt"
 python3 localization_parser.py "decompiled-data\localizations\citadel_gc_english.txt" "decompiled-data\localizations\citadel_gc_english.json"
 
-cp "$DEADLOCK_LOCATION\game\citadel\resource\localization\citadel_mods\citadel_mods_english.txt" "decompiled-data\localizations\citadel_mods_english.txt"
+cp "$DEADLOCK_PATH\game\citadel\resource\localization\citadel_mods\citadel_mods_english.txt" "decompiled-data\localizations\citadel_mods_english.txt"
 python3 localization_parser.py "decompiled-data\localizations\citadel_mods_english.txt" "decompiled-data\localizations\citadel_mods_english.json"
