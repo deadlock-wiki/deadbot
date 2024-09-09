f3182e39ba7df0e4cec1a89872d4a9d1b5e28724
Jason Lang <raptorify@gmail.com>
2024-09-10 00:43:07 +0200

    Add OS check to determine the decompiler executable

--- src/parser/decompiler/decompile.sh
+++ src/parser/decompiler/decompile.sh
@@ -8,13 +8,25 @@ FILES=("scripts/heroes" "scripts/abilities" "scripts/generic_data" "scripts/misc
 
 cp "$DEADLOCK_PATH/game/citadel/steam.inf" "decompiled-data/version.txt"
 
-# Loop through files and run Decompiler.exe for each
+ # Detect the operating system
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+      # MacOS
+      DECOMPILER_EXEC="Decompiler"  # Mac executable without .exe
+  elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+      # Windows (via Git Bash or Cygwin)
+      DECOMPILER_EXEC="Decompiler.exe"  # Windows executable with .exe
+  else
+      echo "Unsupported operating system."
+      exit 1
+  fi
+
+# Loop through files and run Decompiler for each
 for FILE in "${FILES[@]}"; do
   INPUT_PATH="$DEADLOCK_PATH/game/citadel/pak01_dir.vpk"
   VPK_FILEPATH="${FILE}.vdata_c"
-  
-  # Run the decompiler
-  "$DECOMPILER_PATH/Decompiler.exe" -i "$INPUT_PATH" --output "$OUTPUT_DIR" --vpk_filepath "$VPK_FILEPATH" -d
+
+# Run the decompiler
+"$DECOMPILER_PATH/$DECOMPILER_EXEC" -i "$INPUT_PATH" --output "$OUTPUT_DIR" --vpk_filepath "$VPK_FILEPATH" -d
 
   # Remove subclass and convert to json
   VDATA_FILE="$OUTPUT_DIR/${FILE}.vdata"
@@ -35,7 +47,7 @@ folders=("citadel_attributes" "citadel_gc" "citadel_heroes" "citadel_main" "cita
 for folder in "${folders[@]}"; do
     # Construct the source path using DEADLOCK_PATH and folder name
     src_path="$DEADLOCK_PATH/game/citadel/resource/localization/$folder"
-    
+
     # Construct the destination path by replacing "citadel_" prefix with ""
     dest_folder_name="${folder#citadel_}"
     dest_path="decompiled-data/localizations/$dest_folder_name"
