c19a8bb592d961decdd3c8e4cc25e86ef7a3be9b
Surxe <119145352+Surxe@users.noreply.github.com>
2024-09-14 23:48:11 -0400

    More weapon stats added

--- src/parser/parsers/heroes.py
+++ src/parser/parsers/heroes.py
@@ -104,7 +104,17 @@ class HeroParser:
             'RoundsPerSecond': 1 / weapon_prim['m_flCycleTime'],
             'ClipSize': weapon_prim['m_iClipSize'],
             'ReloadTime': weapon_prim['m_reloadDuration'],
+            'ReloadMovespeed': int(weapon_prim['m_flReloadMoveSpeed'])/10000,
+            'ReloadDelay': weapon_prim['m_flReloadSingleBulletsInitialDelay'] if 'm_flReloadSingleBulletsInitialDelay' in weapon_prim else 0,
+            'ReloadSingle': weapon_prim['m_bReloadSingleBullets'] if 'm_bReloadSingleBullets' in weapon_prim else False,
             'BulletSpeed': self.calc_bullet_velocity(weapon_prim['m_BulletSpeedCurve']['m_spline']),
+            'FalloffStartRange': weapon_prim['m_flDamageFalloffStartRange']/ENGINE_UNITS_PER_METER,
+            'FalloffEndRange': weapon_prim['m_flDamageFalloffEndRange']/ENGINE_UNITS_PER_METER,
+            'FalloffStartScale': weapon_prim['m_flDamageFalloffStartScale'],
+            'FalloffEndScale': weapon_prim['m_flDamageFalloffEndScale'],
+            'FalloffBias': weapon_prim['m_flDamageFalloffBias'],
+            'BulletGravityScale': weapon_prim['m_flBulletGravityScale'],
+            'BulletRadius': weapon_prim['m_flBulletRadius']/ENGINE_UNITS_PER_METER,
         }
 
         weapon_stats['DPS'] = weapon_stats['BulletDamage'] * weapon_stats['RoundsPerSecond']
