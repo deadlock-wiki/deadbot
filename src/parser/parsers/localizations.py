5d1fe326f633cf1d2c6e247045bdb631bf53c2d5
hassayag <56391012+hassayag@users.noreply.github.com>
2024-09-24 21:10:01 +0100

    Feature/Ability UI parser to store format the parsed hero data into a usable format for display (#27)
    * Created initial ability ui parser
    
    * got all abilities and heroes showing in ability_ui output
    
    * moved ability description from ability parser to ability-ui parser
    
    * added formatting of descriptions in ability info blocks
    
    * moved all handling of upgrade descriptions to ability ui parser
    
    * added types and "other" category
    
    * Added scalers
    
    * Added upgrade scaling attributes
    
    * categorised ability attributes
    
    * added handling of multiple prop blocks
    
    * don't set type if it is None
    
    * normalised use of Key and Name attributes
    
    * Fixed some UNKNOWN in descriptions
    
    * added missing 'Scale' attrs for ability ui
    
    * added documentation of ability ui parser
    
    * migrated most write calls to the main Parser class
    
    * minor comment change
    
    * added hero names to ability ui
    
    * changed attribute groups to be objects instead of arrays
    
    * moved common attrs to top level for easier usage
    
    * moved upgrade description logic to ability ui parser
    
    * added system of writing formatted localizations to the localization file to avoid complicated frontend logic
    
    * removed 'create_description' logic as this will now be handled on the client
    
    * added ability duration as reserved attr
    
    * Exceptions; easy typo fix
    
    * removed extra string # replacement
    
    ---------
    
    Co-authored-by: Surxe <119145352+Surxe@users.noreply.github.com>

--- src/parser/parsers/localizations.py
+++ src/parser/parsers/localizations.py
@@ -5,16 +5,16 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 import maps as maps
 import utils.json_utils as json_utils
-from .constants import OUTPUT_DIR
 
 
 class LocalizationParser:
-    def __init__(self, localization_data):
+    def __init__(self, localization_data, output_dir):
+        self.OUTPUT_DIR = output_dir
         self.localizations_data = localization_data
 
     def run(self):
         for language, language_data in self.localizations_data.items():
             json_utils.write(
-                OUTPUT_DIR + 'localizations/' + language + '.json',
+                self.OUTPUT_DIR + 'localizations/' + language + '.json',
                 json_utils.sort_dict(language_data),
             )
