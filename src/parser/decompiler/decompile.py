2b5dc494c5daf527add43c5a234d3e68ed0c064e
hassayag <haassayag@gmail.com>
2024-09-28 12:13:44 +0100

    added missing load_dotenv calls

--- src/parser/decompiler/decompile.py
+++ src/parser/decompiler/decompile.py
@@ -1,11 +1,14 @@
 import os
 import kv3_to_json
 from localization import process_localization_files
+from dotenv import load_dotenv
 
-DEADLOCK_PATH=os.getenv("DEADLOCK_PATH")
-WORK_DIR=os.getenv("WORK_DIR")
-OUTPUT_DIR=os.getenv("OUTPUT_DIR")
-DECOMPILER_CMD=os.getenv("DECOMPILER_CMD")
+load_dotenv()
+
+DEADLOCK_PATH = os.getenv('DEADLOCK_PATH')
+WORK_DIR = os.getenv('WORK_DIR')
+OUTPUT_DIR = os.getenv('OUTPUT_DIR')
+DECOMPILER_CMD = os.getenv('DECOMPILER_CMD')
 
 # Define paths
 os.makedirs(WORK_DIR, exist_ok=True)
@@ -13,60 +16,50 @@ os.system(f'cp "{DEADLOCK_PATH}/game/citadel/steam.inf" "{WORK_DIR}/version.txt"
 os.system(f'cp "{DEADLOCK_PATH}/game/citadel/steam.inf" "{OUTPUT_DIR}/version.txt"')
 
 # Define files to be decompiled and processed
-files = [
-  "scripts/heroes",
-  "scripts/abilities",
-  "scripts/generic_data",
-  "scripts/misc"
-]
+files = ['scripts/heroes', 'scripts/abilities', 'scripts/generic_data', 'scripts/misc']
 
 # Loop through files and run Decompiler.exe for each
 for file in files:
-  # removes filename from the file path
-  folder_path = '/'.join(str.split(file,'/')[:-1])
-  os.makedirs(WORK_DIR+"/"+folder_path,exist_ok=True)
+    # removes filename from the file path
+    folder_path = '/'.join(str.split(file, '/')[:-1])
+    os.makedirs(WORK_DIR + '/' + folder_path, exist_ok=True)
 
-  input_path = DEADLOCK_PATH+"/game/citadel/pak01_dir.vpk" 
-  VPK_FILEPATH = file + ".vdata_c"
-  # Run the decompiler
-  dec_cmd = ( DECOMPILER_CMD +
-    f' -i "{input_path}" --output "{WORK_DIR}/vdata" --vpk_filepath "{VPK_FILEPATH}" -d'
-  )
-  os.system(dec_cmd)
-  # Remove subclass and convert to json
-  kv3_to_json.process_file(f"{WORK_DIR}/vdata/{file}.vdata", f"{WORK_DIR}/{file}.json")
+    input_path = DEADLOCK_PATH + '/game/citadel/pak01_dir.vpk'
+    VPK_FILEPATH = file + '.vdata_c'
+    # Run the decompiler
+    dec_cmd = (
+        DECOMPILER_CMD
+        + f' -i "{input_path}" --output "{WORK_DIR}/vdata" --vpk_filepath "{VPK_FILEPATH}" -d'
+    )
+    os.system(dec_cmd)
+    # Remove subclass and convert to json
+    kv3_to_json.process_file(f'{WORK_DIR}/vdata/{file}.vdata', f'{WORK_DIR}/{file}.json')
 
 # Define an array of folders to parse
 # All folders (UNUSED)
-all_folders = [ 
-  "citadel_attributes",
-  "citadel_dev",
-  "citadel_gc",
-  "citadel_generated_vo",
-  "citadel_heroes",
-  "citadel_main",
-  "citadel_mods"
-] 
+all_folders = [
+    'citadel_attributes',
+    'citadel_dev',
+    'citadel_gc',
+    'citadel_generated_vo',
+    'citadel_heroes',
+    'citadel_main',
+    'citadel_mods',
+]
 
 # All folders but voice lines and dev for now
-folders = [
-  "citadel_attributes",
-  "citadel_gc",
-  "citadel_heroes",
-  "citadel_main",
-  "citadel_mods"
-]
+folders = ['citadel_attributes', 'citadel_gc', 'citadel_heroes', 'citadel_main', 'citadel_mods']
 
 # Loop through each folder in the array
 for folder in folders:
     # Construct the source path using DEADLOCK_PATH and folder name
-    src_path = f"{DEADLOCK_PATH}/game/citadel/resource/localization/{folder}"
-    
+    src_path = f'{DEADLOCK_PATH}/game/citadel/resource/localization/{folder}'
+
     # Construct the destination path by replacing "citadel_" prefix with ""
-    dest_folder_name = str.replace(folder,"citadel_","")
-    dest_path = f"{WORK_DIR}/localizations/{dest_folder_name}"
-    os.makedirs(dest_path,exist_ok=True)
+    dest_folder_name = str.replace(folder, 'citadel_', '')
+    dest_path = f'{WORK_DIR}/localizations/{dest_folder_name}'
+    os.makedirs(dest_path, exist_ok=True)
 
     # Run the Python script to parse the folder
-    process_localization_files(src_path,dest_path)
-    print(f"Parsed {src_path} to {dest_path}")
+    process_localization_files(src_path, dest_path)
+    print(f'Parsed {src_path} to {dest_path}')
