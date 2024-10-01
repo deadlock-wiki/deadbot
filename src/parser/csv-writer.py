f5132de435c5a7738fc21d137a9e055eff2f6253
b13rg <13488329+B13rg@users.noreply.github.com>
2024-10-01 14:11:08 -0700

    add csvwriter parameter

--- src/parser/csv-writer.py
+++ src/parser/csv-writer.py
@@ -4,11 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OUTPUT_DIR = os.getenv('OUTPUT_DIR', '../output-data')
-
-
-def export_json_file_to_csv(file_name):
-    os.makedirs(f'{OUTPUT_DIR}/json', exist_ok=True)
+def export_json_file_to_csv(file_name, OUTPUT_DIR):
+    os.makedirs(f'{OUTPUT_DIR}/json',exist_ok=True)
     df = pd.read_json(f'{OUTPUT_DIR}/json/{file_name}.json').transpose()
     df = df.applymap(convert_array_to_string)
     os.makedirs(f'{OUTPUT_DIR}/csv', exist_ok=True)
@@ -19,8 +16,3 @@ def convert_array_to_string(value):
     if isinstance(value, list):
         return ', '.join(str(value))
     return value
-
-
-if __name__ == '__main__':
-    export_json_file_to_csv('item-data')
-    export_json_file_to_csv('hero-data')
