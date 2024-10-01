db8457487a505af9c25c9b6b56dd5c8f77d8cb51
b13rg <13488329+B13rg@users.noreply.github.com>
2024-10-01 14:10:20 -0700

    Replace changelog parser env vars

--- src/parser/parsers/changelogs.py
+++ src/parser/parsers/changelogs.py
@@ -8,11 +8,9 @@ import datetime
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import utils.json_utils as json_utils
 
-from .constants import OUTPUT_DIR
-
 
 class ChangelogParser:
-    def __init__(self):
+    def __init__(self, OUTPUT_DIR):
         self.CHANGELOGS_DIR = os.path.join(os.path.dirname(__file__), '../raw-changelogs/')
         self.OUTPUT_DIR = OUTPUT_DIR
         self.OUTPUT_CHANGELOGS = self.OUTPUT_DIR+'/changelogs'
@@ -185,7 +183,3 @@ class ChangelogParser:
             sorted_changelogs[key] = changelogs[key]
 
         return sorted_changelogs
-
-
-if __name__ == '__main__':
-    ChangelogParser().run_all()
