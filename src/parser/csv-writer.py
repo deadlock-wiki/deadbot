8b70e5cc077ab4e5cac9a663adaa6f30a676a4b1
Surxe <119145352+Surxe@users.noreply.github.com>
2024-10-03 23:05:52 -0400

    path changes for csv-writer

--- src/parser/csv-writer.py
+++ src/parser/csv-writer.py
@@ -3,13 +3,14 @@ import os
 
 OUTPUT_DIR = os.getenv('OUTPUT_DIR', '../output-data')
 
-
-def export_json_file_to_csv(file_name):
-    os.makedirs(f'{OUTPUT_DIR}/json', exist_ok=True)
-    df = pd.read_json(f'{OUTPUT_DIR}/json/{file_name}.json').transpose()
+# OUTPUT_DIR/source.json
+# OUTPUT_DIR/csv/dest.csv
+def export_json_file_to_csv(source, dest):
+    os.makedirs(f'{OUTPUT_DIR}', exist_ok=True)
+    df = pd.read_json(f'{OUTPUT_DIR}/{source}.json').transpose()
     df = df.applymap(convert_array_to_string)
     os.makedirs(f'{OUTPUT_DIR}/csv', exist_ok=True)
-    df.to_csv(f'{OUTPUT_DIR}/csv/{file_name}.csv')
+    df.to_csv(f'{OUTPUT_DIR}/csv/{dest}.csv')
 
 
 def convert_array_to_string(value):
@@ -19,5 +20,5 @@ def convert_array_to_string(value):
 
 
 if __name__ == '__main__':
-    export_json_file_to_csv('item-data')
-    export_json_file_to_csv('hero-data')
+    export_json_file_to_csv('resources/Item', 'item-data')
+    export_json_file_to_csv('resources/Hero', 'hero-data')
