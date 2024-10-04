cc06c29656bf714994db435ea7cf7b7801634c29
b13rg <13488329+B13rg@users.noreply.github.com>
2024-10-03 20:12:18 -0700

    ruff format

--- src/deadbot.py
+++ src/deadbot.py
@@ -39,58 +39,6 @@ class DeadBot:
         else:
             print(f"No changes made to '{page.name}'")
 
-def parse_arguments():
-    parser = argparse.ArgumentParser(
-                    prog='DeadBot',
-                    description='Bot that lives to serve deadlocked.wiki',
-                    epilog='Process Deadlock game files and extract data and stats')
-
-
-    # Setup / Base config Flags
-    parser.add_argument(
-        "-i", "--dl_path", 
-        help="Path to Deadlock game files (overrides DEADLOCK_PATH environment variable)", 
-        default=os.getenv('DEADLOCK_PATH'))
-    parser.add_argument(
-        "-w", "--workdir", 
-        help="Directory for temp working files (overrides WORK_DIR environment variable)",
-        default=os.getenv('WORK_DIR',os.path.abspath(os.getcwd())+'/decompiled-data'))
-    parser.add_argument(
-        "-o", "--output", 
-        help="Output directory (overrides OUTPUT_DIR environment variable)", 
-        default=os.getenv('OUTPUT_DIR',os.path.abspath(os.getcwd())+'/output-data'))
-    parser.add_argument(
-        "-", "--decompiler_cmd", 
-        help="Command for Valve Resource Format tool (overrides DECOMPILER_CMD env variable)",
-        default=os.getenv('DECOMPILER_CMD','Decompiler'))    
-
-    # Operational Flags
-    parser.add_argument(
-        "-d", "--decompile", 
-        help="Decompiles Deadlock game files (overrides DECOMPILE environment variable)", 
-        default=os.getenv('DECOMPILE',False))
-    parser.add_argument(
-        "-p", "--parse", 
-        help="Parses decompiled game files into json and csv (overrides PARSE env variable)", 
-        default=os.getenv('PARSE',False))
-    parser.add_argument(
-        "-b", "--bot_push", 
-        help="Push current data to wiki (overrides BOT_PUSH environment variable)", 
-        default=os.getenv('BOT_PUSH',False))
-    parser.add_argument(
-        "--iam_key",
-        help="AWS iam key for updating bucket (overrides IAM_KEY environment variable)", 
-        default=os.getenv('IAM_KEY'))
-    parser.add_argument(
-        "--iam_secret",
-        help="AWS iam secret for updating bucket (overrides IAM_SECRET environment variable)", 
-        default=os.getenv('IAM_SECRET'))
-    parser.add_argument(
-        "--bucket",
-        help="S3 bucket name to push to (overrides BUCKET environment variable)", 
-        default=os.getenv('BUCKET','deadlock-game-files'))
-
-    return parser.parse_args()
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -132,10 +80,32 @@ def parse_arguments():
         help='Decompiles Deadlock game files. (also set with DECOMPILE environment variable)',
     )
     parser.add_argument(
+        '-p',
+        '--parse',
+        help='Parses decompiled game files into json and csv (overrides PARSE env variable)',
+        default=os.getenv('PARSE', False),
+    )
+    parser.add_argument(
         '-b',
         '--bot_push',
         action='store_true',
         help='Push current data to wiki (also set with BOT_PUSH environment variable)',
+    )
+    # s3 config
+    parser.add_argument(
+        '--iam_key',
+        help='AWS iam key for updating bucket (overrides IAM_KEY environment variable)',
+        default=os.getenv('IAM_KEY'),
+    )
+    parser.add_argument(
+        '--iam_secret',
+        help='AWS iam secret for updating bucket (overrides IAM_SECRET environment variable)',
+        default=os.getenv('IAM_SECRET'),
+    )
+    parser.add_argument(
+        '--bucket',
+        help='S3 bucket name to push to (overrides BUCKET environment variable)',
+        default=os.getenv('BUCKET', 'deadlock-game-files'),
     )
 
     return parser.parse_args()
@@ -161,14 +131,14 @@ def main():
         print('! Skipping Decompiler !')
 
     if args.parse or os.getenv('PARSE', False) in true_args:
-        print("Parsing decompiled files...")
-        game_parser = parser.Parser(args.workdir,args.output)
+        print('Parsing decompiled files...')
+        game_parser = parser.Parser(args.workdir, args.output)
         game_parser.run()
-        print("Exporting to CSV...")
+        print('Exporting to CSV...')
         csv_writer.export_json_file_to_csv('item-data', args.output)
         csv_writer.export_json_file_to_csv('hero-data', args.output)
     else:
-        print("! Skipping Parser !")
+        print('! Skipping Parser !')
 
     if args.bot_push or os.getenv('BOT_PUSH', False) in true_args:
         print('Running DeadBot...')
@@ -178,14 +148,10 @@ def main():
         print('! Skipping DeadBot !')
 
     if args.iam_key and args.iam_secret:
-        parser.S3(
-            args.output,
-            args.bucket,
-            args.iam_key,
-            args.iam_secret
-        ).write()
+        parser.S3(args.output, args.bucket, args.iam_key, args.iam_secret).write()
 
-    print("\nDone!")
+    print('\nDone!')
+
 
 if __name__ == '__main__':
     main()
