9257f887a4751de9559ec9f7d32fff5e793f29ae
Surxe <119145352+Surxe@users.noreply.github.com>
2024-10-22 21:35:57 -0400

    Parser updates, ported from another PR

--- src/changelogs/parse_changelogs.py
+++ src/changelogs/parse_changelogs.py
@@ -3,32 +3,32 @@ import datetime
 
 import utils.json_utils as json_utils
 
-
 class ChangelogParser:
     def __init__(self, output_dir):
         self.OUTPUT_DIR = output_dir
         self.OUTPUT_CHANGELOGS = self.OUTPUT_DIR + '/changelogs'
         self.resources = self._get_resources()
-        self.changelogs_by_date = {}
+        self.localization_en = self.get_lang_en()
+
+        self.default_tag = 'Other'
+        self.unique_tags = [self.default_tag]
+        self.unique_tag_groups = [self.default_tag]
 
     def run_all(self, dict_changelogs):
         # take parsed changelogs and transform them into some other useful formats
-        for date, changelog in dict_changelogs.items():
-            self.run(date, changelog)
-        self._create_resource_changelogs()
-        self._create_changelog_db_data()
+        for version, changelog in dict_changelogs.items():
+            self.run(version, changelog)
+
+        print('Unique Tag Groups:', self.unique_tag_groups)
+        #print('Unique Tags:', self.unique_tags)
+
+        self._write_unique_tag_groups(self.unique_tag_groups)
 
     def run(self, version, logs):
         changelog_lines = logs.split('\n')
 
-        current_heading = 'Other'
-        default_category = 'General'
-        changelog_dict = {
-            current_heading: {default_category: []},
-            'Heroes': {default_category: []},
-            'Items': {default_category: []},
-            'Abilities': {default_category: []},
-        }
+        current_heading = ''
+        changelog_out = []
 
         for line in changelog_lines:
             if line is None or line == '':
@@ -37,55 +37,138 @@ class ChangelogParser:
             # if heading is found, update current heading
             if line.startswith('[ '):
                 current_heading = line[2:-2]
-                changelog_dict[current_heading] = {default_category: []}
                 continue
 
             # find if a resource can be assigned a changelog line
-            resource_found = False
+            tags = []
             for resource_key in self.resources:
                 resource = self.resources[resource_key]
                 resource_type = resource['Type']
                 resource_name = resource['Name']
+
                 if resource_name is None:
                     continue
 
+                # Skip disabled items
+                if resource_type == 'Items' and resource.get('Disabled', False):
+                    continue
+
                 if resource_name in line:
-                    resource_found = True
-                    if resource_name not in changelog_dict[resource_type]:
-                        changelog_dict[resource_type][resource_name] = []
+                    tags = self._register_tag(tags, tag=resource_name, is_group_tag=False)
 
-                    # strip redundant prefix as it is already grouped under resource_name
-                    if line.startswith(f'- {resource_name}: '):
-                        line = line.replace(f'{resource_name}: ', '')
+                    # Also register the resource type
+                    tags = self._register_tag(tags, tag=resource_type)
 
-                    changelog_dict[resource_type][resource_name].append(line)
+                    # Also register 'Weapon Items', 'Spirit Items', etc. for item resources
+                    # currently these are also a heading, this check makes it future proof
+                    if resource_type == 'Items':
+                        item_slot = resource['Slot']  # i.e. Tech
+                        localized_item_slot = self.localization_en['CitadelCategory'+item_slot]
+                        slot_str = localized_item_slot + ' Items'
+                        tags = self._register_tag(tags, tag=slot_str)
 
-            if not resource_found:
-                changelog_dict[current_heading]['General'].append(line)
+            # check for other tags
+            # all tags in this are counted as a tag group
+            tags_to_search = ['Map']
+            for tag_to_search in tags_to_search:
+                if tag_to_search in line:
+                    tags = self._register_tag(tags, tag=tag_to_search)
 
-        changelog_with_icons = self._embed_icons(changelog_dict)
+            # Also register heading as a tag
+            if current_heading != '':
+                heading_tag = self._heading_to_tag(current_heading)
+                if heading_tag is not None:
+                    tags = self._register_tag(tags, tag=heading_tag)
+
+            # if no tag is found, assign to default group
+            if len(tags) == 0:
+                tags = self._register_tag(tags, tag=self.default_tag)
+
+            # Replace hyphen with asterisk for bullet points on wiki
+            prefixes = ['- ', ' - ']
+            for prefix in prefixes:
+                if line.startswith(prefix):
+                    line = '* ' + line[len(prefix):]
+            
+            changelog_out.append({'Description': line, 'Tags': tags})
+
+        changelog_with_icons = self._embed_icons(changelog_out)
         os.makedirs(self.OUTPUT_CHANGELOGS, exist_ok=True)
-        json_utils.write(self.OUTPUT_CHANGELOGS + f'/date/{version}.json', changelog_with_icons)
+        json_utils.write(self.OUTPUT_CHANGELOGS + f'/versions/{version}.json', changelog_with_icons)
         return changelog_with_icons
+    
+    def _heading_to_tag(self, heading):
+        """
+        Converts a heading to a tag, i.e. Hero and Heroes don't need to be separate tags,
+        New Items doesn't need to be a tag at all, etc
+        """
 
-    # mass find and replace of any resource names with the ability icon template
+        # Remove ' Changes' suffix, i.e. 'Hero Changes' -> 'Heroes'
+        heading = heading.replace(' Changes', '')
+
+        map = {
+            "Hero Gameplay": "Heroes",
+            "Hero Gamepla": "Heroes",
+            "Hero": "Heroes",
+            "Item Gameplay": "Items",
+            "New Items": "Items",
+            "Misc Gameplay": self.default_tag,
+            "General": self.default_tag,
+            "General Change": self.default_tag
+        }
+
+        # headings in this list are not converted to tags
+        headings_to_ignore = ['Ranked Mode']
+        if heading in headings_to_ignore:
+            return None
+
+        return map.get(heading, heading)
+    
+    def _register_tag(self, tags, tag, is_group_tag=True):
+        """
+        Registers a tag to the changelog's unique current tags, 
+        and to the static unique list of tags.
+        """
+
+        if tag not in tags:
+            tags.append(tag)
+
+        if tag not in self.unique_tags:
+            self.unique_tags.append(tag)
+
+        if is_group_tag and tag not in self.unique_tag_groups:
+            self.unique_tag_groups.append(tag)
+
+        return tags
+
+    # mass find and replace of referenced tags to {{PageRef|tag}}
     def _embed_icons(self, changelog):
-        for header, log_groups in changelog.items():
-            for group_name, logs in log_groups.items():
-                for index, log in enumerate(logs):
-                    for resource_key in self.resources:
-                        resource = self.resources[resource_key]
-                        resource_name = resource['Name']
-                        if resource_name is None:
-                            continue
+        new_changelog = changelog.copy()
+        for index, log in enumerate(changelog):
+            tags = log['Tags']
+            description = log['Description']
+            for tag in tags:
+                template = 'PageRef'
 
-                        if resource_name not in log:
-                            continue
-                        icon = f'{{AbilityIcon|Ability={resource_name}|Size=20}}'
-                        new_log = log.replace(resource_name, f'{icon} {resource_name}')
-                        changelog[header][group_name][index] = new_log
+                icon = '{{' + template + '|' + tag + '}}'
+                description = description.replace(tag, f'{icon} {tag}')
+                new_changelog[index]['Description'] = description
 
         return changelog
+    
+    def _write_unique_tag_groups(self, unique_tag_groups):
+        # Read existing tag groups
+        tag_groups_path = self.OUTPUT_CHANGELOGS + '/tag_groups.json'
+        if os.path.exists(tag_groups_path):
+            existing_tag_groups = json_utils.read(tag_groups_path)
+
+            # Print a warning if the data is different
+            if existing_tag_groups != unique_tag_groups:
+                print('WARNING: Unique tag groups are different from existing tag groups. \n'+
+                    'Clean up any new ones if necessary by referring to ChangelogParser._heading_to_tag()')
+                
+        # Write the new ones to file
+        json_utils.write(tag_groups_path, unique_tag_groups)
 
     def _get_resources(self):
         resources = {}
@@ -107,63 +190,6 @@ class ChangelogParser:
         resources.update(abilities)
 
         return resources
-
-    # Creates historic changelog for each resource (eg. heroes, items etc.)
-    # using each parsed changelog
-    def _create_resource_changelogs(self):
-        hero_changelogs = {}
-        for date, changelog in self.changelogs_by_date.items():
-            for hero, changes in changelog['Heroes'].items():
-                if hero not in hero_changelogs:
-                    hero_changelogs[hero] = {}
-                hero_changelogs[hero][date] = changes
-
-        for hero_name, changelog in hero_changelogs.items():
-            json_utils.write(
-                self.OUTPUT_CHANGELOGS + f'/hero/{hero_name}.json',
-                self._sort_object_by_date_key(changelog),
-            )
-
-        item_changelogs = {}
-        for date, changelog in self.changelogs_by_date.items():
-            for item, changes in changelog['Items'].items():
-                if item not in item_changelogs:
-                    item_changelogs[item] = {}
-                item_changelogs[item][date] = changes
-
-        for item_name, changelog in item_changelogs.items():
-            json_utils.write(
-                self.OUTPUT_CHANGELOGS + f'/item/{item_name}.json',
-                self._sort_object_by_date_key(changelog),
-            )
-
-    # Convert changelogs to an array of rows, with the plan to upload
-    # them to a database (TODO)
-    def _create_changelog_db_data(self):
-        rows = []
-        for date, changelog in self.changelogs_by_date.items():
-            for header, log_groups in changelog.items():
-                for group_name, logs in log_groups.items():
-                    for index, log in enumerate(logs):
-                        changelog_row = {
-                            'id': f'{header}-{group_name}-{index}',
-                            'changelog': log,
-                            'resource_key': group_name,
-                            'resource_type': header,
-                            'patch_version': '',
-                            'timestamp': date,
-                        }
-                        rows.append(changelog_row)
-
-        return rows
-
-    def _sort_object_by_date_key(self, changelogs):
-        sorted_keys = sorted(
-            changelogs.keys(), key=lambda x: datetime.datetime.strptime(x, '%m-%d-%Y')
-        )
-
-        sorted_changelogs = {}
-        for key in reversed(sorted_keys):
-            sorted_changelogs[key] = changelogs[key]
-
-        return sorted_changelogs
+    
+    def get_lang_en(self):
+        return json_utils.read(self.OUTPUT_DIR + '/localizations/english.json')
