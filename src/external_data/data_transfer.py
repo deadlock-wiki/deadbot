5dd3f424891478b10d952da3f22d5a926cd68d82
hassayag <haassayag@gmail.com>
2024-10-24 23:13:56 +0100

    improved documentation of clear_folder

--- src/external_data/data_transfer.py
+++ src/external_data/data_transfer.py
@@ -81,13 +81,13 @@ class DataTransfer:
         return self.s3.get_folders()
 
     def _clear_folder(self, folder):
-        # Remove all files and subdirectories within the folder
+        """Remove all files and subdirectories within the folder"""
         for filename in os.listdir(folder):
             file_path = os.path.join(folder, filename)
             try:
                 if os.path.isfile(file_path) or os.path.islink(file_path):
-                    os.unlink(file_path)  # Remove file or link
+                    os.unlink(file_path)
                 elif os.path.isdir(file_path):
-                    shutil.rmtree(file_path)  # Remove directory
+                    shutil.rmtree(file_path)
             except Exception as e:
-                print(f'Failed to delete {file_path}. Reason: {e}')
+                print(f'[ERROR] Failed to delete {file_path} - {e}')
