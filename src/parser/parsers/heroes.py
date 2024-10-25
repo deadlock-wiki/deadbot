3c7a7b7fc5b5e2234c0b7af45aa164cd93760002
Ethan <119145352+Surxe@users.noreply.github.com>
2024-10-25 17:24:02 -0400

    Default to false
    Co-authored-by: hassayag <56391012+hassayag@users.noreply.github.com>

--- src/parser/parsers/heroes.py
+++ src/parser/parsers/heroes.py
@@ -28,8 +28,7 @@ class HeroParser:
 
                 # Key is missing from released heroes
                 # Frontend will need to use if "m_bAvailableInHeroLabs or not m_bInDevelopment"
-                if 'm_bAvailableInHeroLabs' in hero_value:
-                    hero_stats['InHeroLabs'] = hero_value['m_bAvailableInHeroLabs']
+                hero_stats['InHeroLabs'] = hero_value.get('m_bAvailableInHeroLabs', False)
 
                 hero_stats.update(
                     self._map_attr_names(hero_value['m_mapStartingStats'], maps.get_hero_attr)
