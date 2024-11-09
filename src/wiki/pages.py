42ca789dc54446885907584553fd49b9e2805905
hassayag <haassayag@gmail.com>
2024-11-09 00:48:57 +0000

    functioning proof of concept to upload json data to a test page

--- src/wiki/pages.py
+++ src/wiki/pages.py
@@ -1,5 +1,6 @@
 PAGE_FILE_MAP = {
-    'Data:HeroData.json': 'json/hero-data.json',
-    'Data:AbilityData.json': 'json/ability-data.json',
-    'Data:AbilityCards.json': 'json/ability-cards.json',
+    'DeadbotTest': 'json/hero-data.json'
+    # 'Data:HeroData.json': 'json/hero-data.json',
+    # 'Data:AbilityData.json': 'json/ability-data.json',
+    # 'Data:AbilityCards.json': 'json/ability-cards.json',
 }
--- src/wiki/upload.py
+++ src/wiki/upload.py
@@ -1,5 +1,6 @@
 import os
 import mwclient
+import json
 from utils import json_utils
 from .pages import PAGE_FILE_MAP
 
@@ -13,48 +14,44 @@ class WikiUpload:
         self.OUTPUT_DIR = output_dir
         self.DATA_NAMESPACE = 'Data'
         self.upload_message = 'DeadBot vx.xx buildyyyyy'
+
         self.auth = {
             'user': os.environ.get('BOT_WIKI_USER'),
             'password': os.environ.get('BOT_WIKI_PASS'),
         }
 
-        self.edit_counter = 0
-
-        site = mwclient.Site('deadlocked.wiki', path='/')
-        site.login(self.auth['user'], self.auth['password'])
-
-        # filter for the "Data" namespace, as that is where all generated data lives on the wiki
-        self.data_pages = [
-            page for page in site.pages if self._get_namespace(page.name) == self.DATA_NAMESPACE
-        ]
+        self.site = mwclient.Site('deadlocked.wiki', path='/')
+        self.site.login(self.auth['user'], self.auth['password'])
 
     def update_data_pages(self):
-        for page in self.data_pages:
-            file_path = PAGE_FILE_MAP.get(page.name)
-            if file_path is None:
+        for page in self.site.pages:
+            page_name_obj = self._split_page_name(page.name)
+            namespace = page_name_obj['namespace']
+            page_name = page_name_obj['page_name']
+
+            # filter for the "Data" namespace, as that is where all generated data lives on the wiki
+            if namespace != self.DATA_NAMESPACE:
                 continue
 
-            file = json_utils.read(f'{self.OUTPUT_DIR}/{file_path}')
-            print('Updating page', file)
-            # self._update_page(page)
+            file_path = PAGE_FILE_MAP.get(page_name)
+            if file_path is None:
+                print(f'[WARN] Missing file map for data page "{page_name}"')
+                continue
+
+            data = json_utils.read(f'{self.OUTPUT_DIR}/{file_path}')
+            json_string = json.dumps(data, indent=4)
+            self._update_page(page, json_string)
 
     def _update_page(self, page, updated_text):
         page.save(updated_text, summary=self.upload_message)
         print(f"Page '{page.name}' updated")
 
-    def _get_namespace(self, full_page_name: str):
+    def _split_page_name(self, full_page_name: str):
         """
         Retrieve namespace of page name, where full page name is formatted as '$NAMESPACE:$PAGE_NAME'
         """
         split_page = full_page_name.split(':')
         if len(split_page) != 2:
-            return
+            return {'namespace': 'Main', 'page_name': full_page_name}
 
-        return split_page[0]
-
-    def _page_has_category(page, category_name):
-        for category in page.categories():
-            if category.name == category_name:
-                return True
-
-        return False
+        return {'namespace': split_page[0], 'page_name': split_page[1]}
