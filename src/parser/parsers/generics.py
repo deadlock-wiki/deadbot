64015cc607d7c6bc31627886ec8d6721b403263d
Surxe <119145352+Surxe@users.noreply.github.com>
2024-11-10 11:53:21 -0500

    validate_structures and remove_prefixes now class member funcs

--- src/parser/parsers/generics.py
+++ src/parser/parsers/generics.py
@@ -26,7 +26,7 @@ class GenericParser:
     def run(self):
         # Parse generic data
         possible_prefixes = ['m_str', 'm_map', 'm_n', 'm_fl', 'm_', 'fl', 'E', 'n']
-        parsed_generics = remove_prefixes(self.generic_data, possible_prefixes)
+        parsed_generics = self._remove_prefixes(self.generic_data, possible_prefixes)
 
         # Read existing generic data
         existing_generics = self._read()
@@ -34,7 +34,7 @@ class GenericParser:
         # If there ie existing data, validate the structure
         if existing_generics is not None:
             structure_keys_to_validate = ['ObjectiveParams', 'RejuvParams', 'ItemPricePerTier']
-            invalid_keys = validate_structures(
+            invalid_keys = self._validate_structures(
                 existing_generics, parsed_generics, structure_keys_to_validate
             )
             
@@ -48,80 +48,80 @@ class GenericParser:
         return parsed_generics
 
 
-def validate_structures(datas1, datas2, structure_keys_to_validate):
-    """
-    Validate that the structure (meaning shape and keys, but not value)
-    of the values for each key in datas1 and datas2 match
+    def _validate_structures(self, datas1, datas2, structure_keys_to_validate):
+        """
+        Validate that the structure (meaning shape and keys, but not value)
+        of the values for each key in datas1 and datas2 match
 
-    Returns a hash of all the invalid keys
+        Returns a hash of all the invalid keys
 
-    In consecutive layers, the keys of datas1 are displayed for the invalid keys
-    """
-    invalid_keys = dict()
+        In consecutive layers, the keys of datas1 are displayed for the invalid keys
+        """
+        invalid_keys = dict()
 
-    data_to_test = [[datas1, datas2], [datas2, datas1]]
+        data_to_test = [[datas1, datas2], [datas2, datas1]]
 
-    # Test both ways
-    for datas1, datas2 in data_to_test:
-        for key in datas1.keys():
-            if key not in structure_keys_to_validate:
-                continue
-
-            # Ensure both contain the key
-            value1 = datas1.get(key, None)
-            value2 = datas2.get(key, None)
-            if value1 is None or value2 is None:
-                invalid_keys[key] = 'The key for this value is missing in one of the data sets'
-                continue
-
-            # Check if the values differ, as this must occur first
-            if datas1[key] != datas2[key]:
-                # Ensure the types match
-                type1 = type(datas1[key])
-                type2 = type(datas2[key])
-                if type1 != type2:
-                    invalid_keys[key] = 'The types of the values differ here'
+        # Test both ways
+        for datas1, datas2 in data_to_test:
+            for key in datas1.keys():
+                if key not in structure_keys_to_validate:
                     continue
 
-                # If the value is a dictionary, recursively check the structure
-                if isinstance(datas1[key], dict):
-                    more_invalid_keys = validate_structures(value1, value2, value1.keys())
-                    if len(more_invalid_keys) > 0:
-                        # Add the invalid keys to the current dict
-                        invalid_keys[key] = more_invalid_keys
+                # Ensure both contain the key
+                value1 = datas1.get(key, None)
+                value2 = datas2.get(key, None)
+                if value1 is None or value2 is None:
+                    invalid_keys[key] = 'The key for this value is missing in one of the data sets'
+                    continue
 
-                elif isinstance(datas1[key], list):
-                    # If the value is a list, check the structure of each element that are dictionaries
-                    for i, elem in enumerate(datas1[key]):
-                        if isinstance(elem, dict):
-                            more_invalid_keys = validate_structures(elem, datas2[key][i], elem.keys(), more_invalid_keys)
-                            if len(more_invalid_keys) > 0:
-                                invalid_keys[key] = more_invalid_keys
+                # Check if the values differ, as this must occur first
+                if datas1[key] != datas2[key]:
+                    # Ensure the types match
+                    type1 = type(datas1[key])
+                    type2 = type(datas2[key])
+                    if type1 != type2:
+                        invalid_keys[key] = 'The types of the values differ here'
+                        continue
 
-    return invalid_keys
+                    # If the value is a dictionary, recursively check the structure
+                    if isinstance(datas1[key], dict):
+                        more_invalid_keys = self._validate_structures(value1, value2, value1.keys())
+                        if len(more_invalid_keys) > 0:
+                            # Add the invalid keys to the current dict
+                            invalid_keys[key] = more_invalid_keys
+
+                    elif isinstance(datas1[key], list):
+                        # If the value is a list, check the structure of each element that are dictionaries
+                        for i, elem in enumerate(datas1[key]):
+                            if isinstance(elem, dict):
+                                more_invalid_keys = self._validate_structures(elem, datas2[key][i], elem.keys(), more_invalid_keys)
+                                if len(more_invalid_keys) > 0:
+                                    invalid_keys[key] = more_invalid_keys
+
+        return invalid_keys
 
 
-def remove_prefixes(generic_data, possible_prefixes):
-    """
-    Recursively remove prefixes from keys in a dictionary
-    """
-    new = dict()
-    for key, value in generic_data.items():
-        # Remove prefix from the key
-        for possible_prefix in possible_prefixes:
-            new_key = string_utils.remove_prefix(key, possible_prefix)
-            if new_key != key:
-                # prefix found
-                break
+    def _remove_prefixes(self, generic_data, possible_prefixes):
+        """
+        Recursively remove prefixes from keys in a dictionary
+        """
+        new = dict()
+        for key, value in generic_data.items():
+            # Remove prefix from the key
+            for possible_prefix in possible_prefixes:
+                new_key = string_utils.remove_prefix(key, possible_prefix)
+                if new_key != key:
+                    # prefix found
+                    break
 
-        # If value is a container, recursively remove prefixes
-        if isinstance(value, dict):
-            value = remove_prefixes(value, possible_prefixes)
-        elif isinstance(value, list):
-            for i, elem in enumerate(value):
-                if isinstance(elem, dict):
-                    value[i] = remove_prefixes(elem, possible_prefixes)
+            # If value is a container, recursively remove prefixes
+            if isinstance(value, dict):
+                value = self._remove_prefixes(value, possible_prefixes)
+            elif isinstance(value, list):
+                for i, elem in enumerate(value):
+                    if isinstance(elem, dict):
+                        value[i] = self._remove_prefixes(elem, possible_prefixes)
 
-        new[new_key] = value
+            new[new_key] = value
 
-    return new
+        return new
