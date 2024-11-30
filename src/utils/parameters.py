2cf44755200c322cb52bc0d6916ba129e467c63b
Surxe <119145352+Surxe@users.noreply.github.com>
2024-11-30 18:38:11 -0500

    Fixed file list; skip download if manifest already downloaded

--- src/utils/parameters.py
+++ src/utils/parameters.py
@@ -71,7 +71,7 @@ def arg_group_base(parser):
     )
     group_base.add_argument(
         '--depot_downloader_dir',
-        help='Path to DepotDownloader executable (also set with DEPOT_DOWNLOADER_DIR environment'
+        help='Path to DepotDownloader directory that contains the executable (also set with DEPOT_DOWNLOADER_DIR environment'
         + ' variable)',
         default=os.getenv('DEPOT_DOWNLOADER_DIR', None),
     )
--- src/versioning/depot_downloader.py
+++ src/versioning/depot_downloader.py
@@ -9,25 +9,25 @@ from constants import APP_ID, DEPOT_ID
 
 class DepotDownloader:
     def __init__(
-        self, output_dir, depot_downloader_dir, steam_username, steam_password, verbose=True
+        self, output_dir, depot_downloader_cmd_dir, steam_username, steam_password, verbose=True
     ):
         self.app_id = APP_ID  # deadlock's app_id
         self.depot_id = DEPOT_ID  # the big depot
 
         self.output_dir = output_dir
-        self.depot_downloader_dir = depot_downloader_dir
+        self.depot_downloader_cmd_dir = depot_downloader_cmd_dir
         self.steam_username = steam_username
         self.steam_password = steam_password
         self.verbose = verbose  # future proofing for verbose argument
         self.depot_downloader_output = 'deadlock-data'
-        self.versions = {}
-        self.versions_path = os.path.join(self.output_dir, 'versions.json')
+        self.manifest_path = os.path.join(self.depot_downloader_output, 'manifest.txt')
 
     def _download(self, manifest_id):
+        result = None
         try:
             # Run the depot_downloader command
             subprocess_params = [
-                os.path.join(self.depot_downloader_dir, './DepotDownloader'),
+                os.path.join(self.depot_downloader_cmd_dir, './DepotDownloader'),
                 '-app',
                 self.app_id,
                 '-depot',
@@ -60,23 +60,48 @@ class DepotDownloader:
                 raise Exception(f'Fatal error: {steam_inf_path} not found')
 
         except Exception as e:
+            #if result is not None:
+                #print('DepotDownloader output: ' + result.stdout)
             raise Exception(f'Error occured while parsing manifest {manifest_id}, error: {e}')
             # Possible errors that cause exceptions include:
             # RateLimiting by Steam
             # Manifest download being blocked by developers
             # Manifests before August 10th, 2024 are not available as they are under NDA
 
-    def _clear_work_dir(self):
-        # Clear the DepotDownloader's work directory, but not output directory
+    def _clear_dl_data(self):
+        # Clear downloaded data, but not the manifest.txt
+        dirs = ['.DepotDownloader', 'game']
+        for dir in dirs:
+            dir_to_rm = os.path.join(self.depot_downloader_output, dir)
+            if os.path.exists(dir_to_rm):
+                shutil.rmtree(dir_to_rm)
 
-        work_dir = os.path.join(self.depot_downloader_output, '.DepotDownloader')
-        if os.path.exists(work_dir):
-            shutil.rmtree(work_dir)
+                if self.verbose:
+                    print(f'Cleared {dir_to_rm}')
 
-            if self.verbose:
-                print(f'Cleared {work_dir}')
+    def _read_downloaded_manifest_id(self):
+        if not os.path.exists(self.manifest_path):
+            return None
+        
+        with open(self.manifest_path, 'r') as f:
+            return f.read().strip()
+        
+    def _write_downloaded_manifest_id(self, manifest_id):
+        with open(self.manifest_path, 'w') as f:
+            f.write(manifest_id)
 
     def run(self, manifest_id):
-        current_game_version = load_game_info(os.path.join(self.output_dir, 'version.txt'))
+        # Check if the manifest is already downloaded
+        downloaded_manifest_id = self._read_downloaded_manifest_id()
+        if downloaded_manifest_id == manifest_id:
+            print(f'Already downloaded manifest {manifest_id}')
+            return
 
+        # Clear the existing data
+        self._clear_dl_data()
+
+        # Download the manifest
         self._download(manifest_id)
+
+        # Write the downloaded manifest id
+        self._write_downloaded_manifest_id(manifest_id)
