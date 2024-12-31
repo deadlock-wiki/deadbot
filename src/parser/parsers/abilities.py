1cae355775424be1593587360a4b4fafd76160c8
Ethan <119145352+Surxe@users.noreply.github.com>
2024-12-31 17:05:37 -0600

    Disabled key defaulted back to False for abils/items

--- src/parser/parsers/abilities.py
+++ src/parser/parsers/abilities.py
@@ -26,7 +26,7 @@ class AbilityParser:
             ability_data = {
                 'Key': ability_key,
                 'Name': self.localizations.get(ability_key, None),
-                'IsDisabled': ability.get('m_bDisabled', None),
+                'IsDisabled': ability.get('m_bDisabled', False),
             }
 
             stats = ability['m_mapAbilityProperties']
