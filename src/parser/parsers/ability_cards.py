d6f83430e8897f6955cb8586a94658588b4f13ee
hassayag <haassayag@gmail.com>
2025-08-27 23:12:04 +0100

    fix: disabled schinese (temporarily) as a supported language as valve have some problematic localization files

--- src/parser/parsers/ability_cards.py
+++ src/parser/parsers/ability_cards.py
@@ -2,7 +2,7 @@ import parser.maps as maps
 import utils.string_utils as string_utils
 from loguru import logger
 
-SUPPORTED_LANGS = ['english', 'russian', 'schinese']
+SUPPORTED_LANGS = ['english', 'russian']
 
 
 class AbilityCardsParser:
