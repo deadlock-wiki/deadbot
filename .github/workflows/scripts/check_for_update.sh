2b6a5e3a33c3257e66ae5f313fc5953cd8c6efb6
hassayag <56391012+hassayag@users.noreply.github.com>
2025-09-06 22:39:32 +0100

    ci: added scheduled job for automatic deployment (#210)

---
+++ .github/workflows/scripts/check_for_update.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Compares the Deadlock client versions between deadlock-data master and the latest on steamdb
+# If steamdb has a later version available, it outputs the new version number
+
+# local repository paths for steamdb and deadlock-data
+STEAMDB=$1
+DEADLOCK_DATA=$2
+
+# Read the file and extract ClientVersion
+latest_inf_file="${STEAMDB}/game/citadel/steam.inf"
+if [[ -f "$latest_inf_file" ]]; then
+    client_version=$(grep '^ClientVersion=' "$latest_inf_file" | cut -d'=' -f2)
+else
+    echo "steam.inf file not found at $latest_inf_file"
+    exit 1
+fi
+
+# same thing for deadlock data
+deployed_inf_file="${DEADLOCK_DATA}/data/version.txt"
+if [[ -f "$deployed_inf_file" ]]; then
+    client_version=$(grep '^ClientVersion=' "$deployed_inf_file" | cut -d'=' -f2)
+else
+    echo "version.txt file not found at $deployed_inf_file"
+    exit 1
+fi
+
+latest_version=$(grep '^ClientVersion=' $latest_inf_file | cut -d'=' -f2)
+deployed_version=$(grep '^ClientVersion=' $deployed_inf_file | cut -d'=' -f2)   
+
+if [[ "$latest_version" == "$deployed_version" ]]; then
+    echo $latest_version
+    exit 0
+else
+    exit 0
+fi
