4a3a78cf836b23d11f904729b2c9a0903b0ef1fd
hassayag <haassayag@gmail.com>
2025-09-08 21:28:55 +0100

    ci: improved error handling update check script

--- .github/workflows/scripts/check_for_update.sh
+++ .github/workflows/scripts/check_for_update.sh
@@ -12,7 +12,7 @@ latest_inf_file="${STEAMDB}/game/citadel/steam.inf"
 if [[ -f "$latest_inf_file" ]]; then
     client_version=$(grep '^ClientVersion=' "$latest_inf_file" | cut -d'=' -f2)
 else
-    echo "steam.inf file not found at $latest_inf_file"
+    echo "Error: steam.inf file not found at $latest_inf_file" >&2
     exit 1
 fi
 
@@ -21,12 +21,22 @@ deployed_inf_file="${DEADLOCK_DATA}/data/version.txt"
 if [[ -f "$deployed_inf_file" ]]; then
     client_version=$(grep '^ClientVersion=' "$deployed_inf_file" | cut -d'=' -f2)
 else
-    echo "version.txt file not found at $deployed_inf_file"
+    echo "Error: version.txt file not found at $deployed_inf_file" >&2
     exit 1
 fi
 
 latest_version=$(grep '^ClientVersion=' $latest_inf_file | cut -d'=' -f2)
 deployed_version=$(grep '^ClientVersion=' $deployed_inf_file | cut -d'=' -f2)   
+
+if [[ -z "$latest_version" ]]; then
+    echo "Error: Latest version not found in $latest_inf_file" >&2
+    exit 1
+fi
+
+if [[ -z "$deployed_version" ]]; then
+    echo "Error: Deployed version not found in $deployed_inf_file" >&2
+    exit 1
+fi
 
 if [[ "$latest_version" == "$deployed_version" ]]; then
     echo ""
