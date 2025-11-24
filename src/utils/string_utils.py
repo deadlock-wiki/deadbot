c8c98acf2c568ce3af3947582eeb20b178390391
LVL1024 <70866179+LVL1024@users.noreply.github.com>
2025-11-24 16:28:42 +0000

    fix: round description variables to 3 sig figs and remove trailing zeros

--- src/utils/string_utils.py
+++ src/utils/string_utils.py
@@ -62,7 +62,12 @@ def _replace_variables(desc, data):
             # strip out units of measure to prevent duplicates eg. "Cooldown reduced by 5ss"
             stripped_value = num_utils.remove_uom(value)
             if type(stripped_value) in [float, int]:
-                return str(stripped_value)
+                rounded_value = num_utils.round_sig_figs(stripped_value, 3)
+
+                if isinstance(rounded_value, float) and rounded_value.is_integer():
+                    return str(int(rounded_value))
+
+                return str(rounded_value)
 
             return value
 
