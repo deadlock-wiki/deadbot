fbfe43e11095c9d27ed131071863e375125f5fda
hassayag <hazzarius@gmail.com>
2026-02-26 12:27:13 +0000

    feat: boiler plate for new item card parser

---
+++ src/parser/parsers/item_cards.py
@@ -0,0 +1,7 @@
+class ItemCardParser:
+    def __init__(self, parsed_items, abilities):
+        self.parsed_items = parsed_items
+        self.abilities = abilities
+
+    def run():
+        return None
