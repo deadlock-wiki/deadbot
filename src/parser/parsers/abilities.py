5c75b8ae76ebdff36058678c11c360d3f63d24b4
hassayag <haassayag@gmail.com>
2026-03-05 22:44:07 +0000

    fix: add support for multiple upgrade scale types for a single prop

--- src/parser/parsers/abilities.py
+++ src/parser/parsers/abilities.py
@@ -67,62 +67,72 @@ class AbilityParser:
         upgrade_sets = ability['m_vecAbilityUpgrades']
         parsed_upgrade_sets = []
 
-        for index, upgrade_set in enumerate(upgrade_sets):
-            parsed_upgrade_set = {}
-
+        for upgrade_set in upgrade_sets:
             upgrades = upgrade_set.get('m_vecPropertyUpgrades')
             if upgrades is None:
                 continue
 
+            # 1. Extract raw data from each upgrade entry
+            raw_upgrades = []
             for upgrade in upgrades:
-                prop = None
-                value = None
-                upgrade_type = None
-                scale_type = None
+                prop = upgrade.get('m_strPropertyName')
+                raw_value = upgrade.get('m_strBonus')
+                upgrade_type = upgrade.get('m_eUpgradeType')
+                scale_type = upgrade.get('m_eScaleStatFilter')
 
-                for key in upgrade:
-                    match key:
-                        case 'm_strPropertyName':
-                            prop = upgrade[key]
+                value = num_utils.assert_number(raw_value)
 
-                        case 'm_strBonus':
-                            value = num_utils.assert_number(upgrade[key])
-
-                        case 'm_eUpgradeType':
-                            upgrade_type = upgrade[key]
-
-                        case 'm_eScaleStatFilter':
-                            scale_type = upgrade[key]
-
-                # if stat has a base value, convert the upgrade value to the appropriate units
                 stat = json_utils.deep_get(ability, 'm_mapAbilityProperties', prop)
                 if stat:
-                    value = self._convert_stat(stat, upgrade[key], value)
+                    value = self._convert_stat(stat, raw_value, value)
 
-                if upgrade_type in ['EAddToBase', None]:
-                    if parsed_upgrade_set.get(prop) is None:
-                        parsed_upgrade_set[prop] = value
-                    # if it is a dict (ie. for a scale value), assign the base value
-                    elif isinstance(parsed_upgrade_set.get(prop), dict):
-                        parsed_upgrade_set[prop]['Value'] = value
-
-                elif upgrade_type in ['EAddToScale', 'EMultiplyScale']:
-                    # If no explicit scale type, look up from the base property
-                    if scale_type is None:
-                        base_stat = json_utils.deep_get(ability, 'm_mapAbilityProperties', prop)
-                        if base_stat and 'm_subclassScaleFunction' in base_stat:
-                            scale_func = base_stat['m_subclassScaleFunction']
-                            scale_type = scale_func.get('m_eSpecificStatScaleType')
-                            # Infer from scale function class if no specific type
-                            if scale_type is None and 'tech' in scale_func.get('_class', ''):
-                                scale_type = 'ETechPower'
-                    parsed_upgrade_set[prop] = {
-                        'Value': parsed_upgrade_set.get(prop, 0),
-                        'Scale': {
-                            'Value': value,
-                            'Type': maps.get_scale_type(scale_type),
-                        },
+                raw_upgrades.append(
+                    {
+                        'prop': prop,
+                        'value': value,
+                        'upgrade_type': upgrade_type,
+                        'scale_type': scale_type,
                     }
+                )
+
+            # 2. Group by prop so we can reason about base + scales together
+            upgrades_by_prop = {}
+            for raw in raw_upgrades:
+                upgrades_by_prop.setdefault(raw['prop'], []).append(raw)
+
+            # 3. Build the parsed upgrade set from grouped data
+            parsed_upgrade_set = {}
+            for prop, entries in upgrades_by_prop.items():
+                base_value = 0
+                scales = []
+
+                for entry in entries:
+                    if entry['upgrade_type'] in ['EAddToBase', None]:
+                        base_value = entry['value']
+                    elif entry['upgrade_type'] in ['EAddToScale', 'EMultiplyScale']:
+                        scale_type = entry['scale_type']
+                        if scale_type is None:
+                            base_stat = json_utils.deep_get(ability, 'm_mapAbilityProperties', prop)
+                            if base_stat and 'm_subclassScaleFunction' in base_stat:
+                                scale_func = base_stat['m_subclassScaleFunction']
+                                scale_type = scale_func.get('m_eSpecificStatScaleType')
+                                if scale_type is None and 'tech' in scale_func.get('_class', ''):
+                                    scale_type = 'ETechPower'
+                        scales.append(
+                            {
+                                'Value': entry['value'],
+                                'Type': maps.get_scale_type(scale_type),
+                            }
+                        )
+
+                if scales:
+                    # if there are multiple scales for a prop, output as an array
+                    if len(scales) > 1:
+                        parsed_upgrade_set[prop] = {'Value': base_value, 'Scale': scales}
+                    else:
+                        parsed_upgrade_set[prop] = {'Value': base_value, 'Scale': scales[0]}
+                else:
+                    parsed_upgrade_set[prop] = base_value
 
             parsed_upgrade_sets.append(parsed_upgrade_set)
 
