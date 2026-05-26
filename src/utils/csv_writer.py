501351567ea308b8636ff23ef29ae26522e7b01b
hassayag <hazzarius@gmail.com>
2026-05-26 12:13:01 +0100

    fix: use proper json strings on csv output

--- src/utils/csv_writer.py
+++ src/utils/csv_writer.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import os
+import json
 
 
 def export_json_file_to_csv(file_name, OUTPUT_DIR):
     os.makedirs(f'{OUTPUT_DIR}/json', exist_ok=True)
     df = pd.read_json(f'{OUTPUT_DIR}/json/{file_name}.json').transpose()
-    df = df.map(convert_array_to_string)
+    df = df.map(convert_value)
     os.makedirs(f'{OUTPUT_DIR}/csv', exist_ok=True)
     df.to_csv(f'{OUTPUT_DIR}/csv/{file_name}.csv')
 
 
-def convert_array_to_string(value):
+def convert_value(value):
     if isinstance(value, list):
-        print(value)
-        return ', '.join(value)
+        return ', '.join(str(v) for v in value)
+    if isinstance(value, dict):
+        return json.dumps(value)
     return value
