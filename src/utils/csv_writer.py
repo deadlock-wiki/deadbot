1b3de1e18c81135f83072a947ea46c6f3d5a0544
Deadbot0 <deadbot1101@gmail.com>
2026-06-04 20:37:43 +0100

    refactor: replaced csv export pandas use with a native implementation

--- src/utils/csv_writer.py
+++ src/utils/csv_writer.py
@@ -1,17 +1,25 @@
-# import pandas as pd
-# import os
+import csv
+import json
+import os
 
 
 def export_json_file_to_csv(file_name, OUTPUT_DIR):
-    return
-    # os.makedirs(f'{OUTPUT_DIR}/json', exist_ok=True)
-    # df = pd.read_json(f'{OUTPUT_DIR}/json/{file_name}.json').transpose()
-    # df = df.map(convert_array_to_string)
-    # os.makedirs(f'{OUTPUT_DIR}/csv', exist_ok=True)
-    # df.to_csv(f'{OUTPUT_DIR}/csv/{file_name}.csv')
+    os.makedirs(f'{OUTPUT_DIR}/json', exist_ok=True)
+    with open(f'{OUTPUT_DIR}/json/{file_name}.json') as f:
+        data = json.load(f)
+
+    os.makedirs(f'{OUTPUT_DIR}/csv', exist_ok=True)
+    if data:
+        all_keys = dict.fromkeys(k for row in data.values() for k in row.keys())
+        fieldnames = [''] + list(all_keys)
+        with open(f'{OUTPUT_DIR}/csv/{file_name}.csv', 'w', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames, extrasaction='ignore')
+            writer.writeheader()
+            for row_key, row in data.items():
+                writer.writerow({'': row_key, **{k: convert_array_to_string(v) for k, v in row.items()}})
 
 
-# def convert_array_to_string(value):
-#     if isinstance(value, list):
-#         return ', '.join(str(value))
-#     return value
+def convert_array_to_string(value):
+    if isinstance(value, list):
+        return ', '.join(str(v) for v in value)
+    return value
